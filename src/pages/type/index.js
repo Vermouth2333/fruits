b03866f7dc433ba763268ed22bf8a20896c68113
fangfei <ganle2233@163.com>
2020-07-16 07:49:18 +0800

    备注

--- src/pages/type/index.js
+++ src/pages/type/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import {Page} from "@common/commonStyled"
 
-import Header from "../../components/xrrcomponents/header"
+import Header from "../../components/xrrcomponents/header" // 123
 import KindsList from "../../components/xrrcomponents/kindslist"
 export default class Type extends Component {
     render() {
